aa0963ee37bf769da88cdbda4cbbe62257eab7ce
Eemil <eemil.korkka@koudata.fi>
2023-02-03 10:51:26 +0200

    Update

--- src/Program.cs
+++ src/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Test 
 {
+    class Car 
+    {
+        public string color;
+        public int modelYear;
+        public int numberOfSeats;
+
+        public Car(string color, int modelYear, int numberOfSeats) 
+        {
+            this.color = color;
+            this.modelYear = modelYear;
+            this.numberOfSeats = numberOfSeats;
+        }
+    }
     class Program 
     {
         static void Main(string[] args) 
         {
-            Console.WriteLine("Hello World!");
+            Car car = new Car("Red", 2005, 4);
+
+            Console.WriteLine("Car color: " + car.color + " Car model: " + car.modelYear + " Number of seats: " + car.numberOfSeats);
         }
     }
 }
