4890040f2086e758966ae7bb9eeaf2bfdc89f01a
Eemil <eemil.korkka@koudata.fi>
2023-02-03 09:11:14 +0200

    Hello World!

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace helloworld 
+{
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
